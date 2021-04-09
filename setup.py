91e8fcb38b4885e60fa8189cf8100c5e1259451c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-04-09 17:52:39 +0100

    update to pytest v6 API

--- setup.py
+++ setup.py
@@ -10,49 +10,49 @@ __version__ = "1.0.0rc5"
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
-    return codecs.open(file_path, encoding='utf-8').read()
+    return codecs.open(file_path, encoding="utf-8").read()
 
 
 def getversion():
-    if 'BUILD_VERSION' in os.environ:
-        return os.environ['BUILD_VERSION']
+    if "BUILD_VERSION" in os.environ:
+        return os.environ["BUILD_VERSION"]
     else:
         return __version__
 
 
 setup(
-    name='pytest-azurepipelines',
+    name="pytest-azurepipelines",
     version=getversion(),
-    author='Anthony Shaw',
-    author_email='anthonyshaw@apache.org',
-    maintainer='Anthony Shaw',
-    maintainer_email='anthonyshaw@apache.org',
-    license='MIT',
-    url='https://github.com/tonybaloney/pytest-azurepipelines',
-    description='Formatting PyTest output for Azure Pipelines UI',
-    long_description=read('README.rst'),
-    py_modules=['pytest_azurepipelines'],
-    python_requires='>=3.5',
-    data_files=[('resources', ['resources/style.css'])],
-    install_requires=['pytest>=5.0.0', 'pytest-nunit>=0.5.3'],
+    author="Anthony Shaw",
+    author_email="anthonyshaw@apache.org",
+    maintainer="Anthony Shaw",
+    maintainer_email="anthonyshaw@apache.org",
+    license="MIT",
+    url="https://github.com/tonybaloney/pytest-azurepipelines",
+    description="Formatting PyTest output for Azure Pipelines UI",
+    long_description=read("README.rst"),
+    py_modules=["pytest_azurepipelines"],
+    python_requires=">=3.6",
+    data_files=[("resources", ["resources/style.css"])],
+    install_requires=["pytest>=6.0.0", "pytest-nunit>=0.5.3"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Pytest',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 4 - Beta",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
     ],
     entry_points={
-        'pytest11': [
-            'azurepipelines = pytest_azurepipelines',
+        "pytest11": [
+            "azurepipelines = pytest_azurepipelines",
         ],
     },
 )
